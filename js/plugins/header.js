a0dc9f14a2ad2c095a668c842e5bfd1f2e377203
Jordan Scales <scalesjordan@gmail.com>
2015-04-12 21:42:10 -0700

    added proper tagging for header plugin

--- js/plugins/header.js
+++ js/plugins/header.js
@@ -5,6 +5,8 @@
 var $ = require("jquery");
 var Plugin = require("./plugin-base");
 
+require("../../less/tag.less");
+
 class Header extends Plugin {
     getTitle() {
         return "Headers";
@@ -14,16 +16,37 @@ class Header extends Plugin {
         return "Highlights headers (<h1>, <h2>, etc) and order violations";
     }
 
+    /**
+     * Tags a particular jQuery element with its tagName
+     */
+    tag($el) {
+        var tagName = $el.prop("tagName").toLowerCase();
+
+        // We'll position the tag absolutely to its nearest positioned
+        // ancestor
+        var $offsetParent = $el.offsetParent();
+        var { top, left } = $el.position();
+
+        var $tag = $("<span>")
+            .addClass("tota11y-tag")
+            .css({
+                top: top + parseFloat($el.css("margin-top")),
+                left: left
+            })
+            .text(tagName);
+
+        $offsetParent.append($tag);
+    }
+
     run() {
-        $("h1, h2, h3, h4, h5, h6").css({
-            "border": "1px solid #000"
+        var _tag = this.tag;
+        $("h1, h2, h3, h4, h5, h6").each(function() {
+            _tag($(this));
         });
     }
 
     cleanup() {
-        $("h1, h2, h3, h4, h5, h6").css({
-            "border": "none"
-        });
+        $(".tota11y-tag").remove();
     }
 }
 
