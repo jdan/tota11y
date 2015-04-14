c8ab714cf75b880c67e3030e76db14f274f4dc96
Jordan Scales <scalesjordan@gmail.com>
2015-04-13 19:22:41 -0700

    added info panel and styles

--- js/plugins/plugin-base.js
+++ js/plugins/plugin-base.js
@@ -50,11 +50,11 @@ class Plugin {
             // You can probably change the class based on active being true
             // or not
             if (this.active) {
-                var $infoHtml = this.run();
+                var infoHtml = this.run();
 
-                if ($infoHtml) {
+                if (infoHtml) {
                     this.$infoContainer
-                        .append($infoHtml)
+                        .html(infoHtml)
                         .addClass("active");
                 }
             } else {
