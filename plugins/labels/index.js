b618ead9dadd614c4569a6091571eea5fece4c13
Jordan Scales <scalesjordan@gmail.com>
2015-05-26 12:46:47 -0400

    replace logic for checking control labels with accessibility developer tools

--- plugins/labels/index.js
+++ plugins/labels/index.js
@@ -2,122 +2,48 @@
  * A plugin to identify unlabeled inputs
  */
 
-var $ = require("jquery");
-var Plugin = require("../base");
-var annotate = require("../shared/annotate")("labels");
+let $ = require("jquery");
+let Plugin = require("../base");
+let annotate = require("../shared/annotate")("labels");
+let audit = require("../shared/audit");
+let InfoPanel = require("../shared/info-panel");
 
 require("./style.less");
 
 const HIGHLIGHT_CLASS = "tota11y-label-highlight";
-const WRAPPING_CLASS = "tota11y-label-wrapped";
+const RULE_NAME = "controlsWithoutLabel";
 
 class LabelsPlugin extends Plugin {
     getTitle() {
-        return "Labels"
+        return "Labels";
     }
 
     getDescription() {
-        return "Identifies inputs with missing labels"
-    }
-
-    // Tests if a given nullable string has any content that can be read by
-    // a screen reader
-    isReadable(text) {
-        return /\w/.test(text || "");
-    }
-
-    // Test for inputs that require a label
-    // Specifically, test for the existence and usefulness of any of the
-    // following:
-    //   - a label whose `for` attribute matches the input's `id`
-    //   - a label wrapping the input
-    //   - an `aria-label` attribute
-    //   - an element whose `id` matches the input's `aria-labelledby`
-    hasValidLabel($el) {
-        // Check for a label tag whose `for` matches the `id` of the input
-        let id = $el.prop("id");
-        if (id && $(`label[for="${id}"]`).text()) {
-            return true;
-        }
-
-        // Check if the input is wrapped in a label
-        if (this.isReadable($el.closest("label").text())) {
-            return true;
-        }
-
-        // Check for an `aria-label` attribute
-        if (this.isReadable($el.prop("aria-label"))) {
-            return true;
-        }
-
-        // Check for an element whose `id` matches the input's
-        // `aria-labelledby` value
-        let labelledBy = $el.prop("aria-labelledby");
-        let $label = $("#" + labelledBy);
-        if ($label && this.isReadable($label.text())) {
-            return true;
-        }
-
-        // TODO: aria-describedby?
-
-        return false;
+        return "Identifies inputs with missing labels";
     }
 
     run() {
-        const assert = ($el, condition, message) => {
-            if (!condition) {
-                // Wrap the element in a span so we can show a tooltip on
-                // hover. We will need to undo this in `cleanup()`
-                $el.addClass(WRAPPING_CLASS).wrap(
-                    $("<span>").addClass("tota11y-tooltipped")
-                               .attr("data-content", message));
+        this.panel = new InfoPanel(this.getTitle());
+        let result = audit("controlsWithoutLabel");
+
+        if (result.result === "FAIL") {
+            result.elements.forEach((element) => {
+                let $el = $(element);
 
                 annotate.highlight($el).addClass(HIGHLIGHT_CLASS);
-            }
+                this.panel.addError(
+                    "Input is missing a label",
+                    "Wrap this in a label tag",
+                    $el);
+            });
         }
 
-        const $inputs = $("button, input, select, textarea");
-
-        $inputs.each((i, el) => {
-            const $el = $(el);
-
-            // Ignore inputs inside of tota11y itself
-            if ($el.parents(".tota11y-toolbar").length > 0) {
-                return;
-            }
-
-            // Ignore disabled inputs and inputs with `type="hidden"`
-            if ($el.is("[disabled], [type='hidden']")) {
-                return;
-            }
-
-            // Submit and reset inputs provide their own labeling
-            if ($el.is("[type='submit'], [type='reset']")) {
-                return;
-            }
-
-            if ($el.is("button")) {
-                return assert($el, this.isReadable($el.text()),
-                    "This button does not contain any inner text");
-            }
-
-            if ($el.is("[type='image']")) {
-                return assert($el, this.isReadable($el.attr("alt")),
-                    "Image inputs must have non-empty alt-text associated " +
-                    "with them");
-            }
-
-            // This element requires a valid label
-            return assert($el, this.hasValidLabel($el),
-                "This input does not have a valid label associated with it");
-        });
+        this.panel.render();
     }
 
     cleanup() {
         annotate.removeAll();
-
-        // Unwrap any wrapped items
-        $("." + WRAPPING_CLASS).removeClass(WRAPPING_CLASS).unwrap();
+        this.panel.destroy();
     }
 }
 
---
+++ plugins/shared/audit.js
@@ -0,0 +1,33 @@
+/**
+ * Abstractions for how we use Accessibility Developer Tools
+ */
+
+function allRuleNames() {
+    return axs.AuditRules.getRules().map(rule => rule.name);
+}
+
+// Creates an audit configuration that whitelists a single rule and limits the
+// amount of tests to run
+function createWhitelist(ruleName) {
+    var config = new axs.AuditConfiguration();
+    config.showUnsupportedRulesWarning = false;
+
+    allRuleNames().forEach((name) => {
+        if (name !== ruleName) {
+            config.ignoreSelectors(name, "*");
+        }
+    });
+
+    return config;
+}
+
+// Audits for a single rule (by name) and returns the results for only that
+// rule
+function audit(ruleName) {
+    let whitelist = createWhitelist(ruleName);
+
+    return axs.Audit.run(whitelist)
+        .filter(result => result.rule.name === ruleName)[0];
+}
+
+module.exports = audit;
