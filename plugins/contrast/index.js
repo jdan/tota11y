58d5d7a8b69e2324a0605db01e776a4106549523
Jordan Scales <scalesjordan@gmail.com>
2015-06-02 16:26:29 -0400

    Detailed error messages for the contrast plugin.
    Summary:
    This changeset makes use of the info panel for the contrast plugin.
    
    Here's what an error message looks like:
    
    I also trimmed out the inline-error message, which will, in the future, link
    to the detailed one:
    
    Test Plan:
    . Activate the "Contrast" plugin
    . See the detailed error message with suggestions of a fix
    
    Reviewers: riley
    
    Differential Revision: https://phabricator.khanacademy.org/D18366

--- plugins/contrast/index.js
+++ plugins/contrast/index.js
@@ -7,6 +7,11 @@ let $ = require("jquery");
 let Plugin = require("../base");
 let annotate = require("../shared/annotate")("labels");
 
+let titleTemplate = require("./error-title.handlebars");
+let descriptionTemplate = require("./error-description.handlebars");
+
+require("./style.less");
+
 class ContrastPlugin extends Plugin {
     getTitle() {
         return "Contrast";
@@ -14,6 +19,34 @@ class ContrastPlugin extends Plugin {
 
     getDescription() {
         return "Labels elements with insufficient contrast";
+    }
+
+    _registerError(options, el) {
+        // Suggest colors
+        let suggestedColors = axs.utils.suggestColors(
+            options.bgColor,
+            options.fgColor,
+            options.contrastRatio,
+            options.style).AA;
+
+        // We'll suggest colors at an "AA" level.
+        // 3.0 for large fonts, 4.5 for small fonts
+        let requiredRatio = axs.utils.isLargeFont(options.style) ? 3.0 : 4.5;
+
+        let templateData = {
+            fgColorHex: axs.utils.colorToString(options.fgColor),
+            bgColorHex: axs.utils.colorToString(options.bgColor),
+            contrastRatio: options.contrastRatio,
+            requiredRatio: requiredRatio,
+            suggestedFgColorHex: suggestedColors.fg,
+            suggestedBgColorHex: suggestedColors.bg,
+            suggestedColorsRatio: suggestedColors.contrast
+        };
+
+        this.error(
+            titleTemplate(templateData),
+            descriptionTemplate(templateData),
+            $(el));
     }
 
     run() {
@@ -42,33 +75,38 @@ class ContrastPlugin extends Plugin {
             let fgColor = axs.utils.getFgColor(style, el);
             let bgColor = axs.utils.getBgColor(style, el);
             let contrastRatio = axs.utils.calculateContrastRatio(
-                fgColor, bgColor);
+                fgColor, bgColor).toFixed(2);
 
-            if (axs.utils.isLowContrast(contrastRatio, style)) {
-                // Suggest colors
-                // TODO: Intelligently use fg/bg dependending on context
-                let suggestedBgColor =
-                    axs.utils.suggestColors(
-                        bgColor, fgColor, contrastRatio, style).AA.bg;
+            // Build a value for our `seenColors` set and report the color
+            // if we have not seen it yet
+            let entry = axs.utils.colorToString(fgColor) + "/" +
+                        axs.utils.colorToString(bgColor);
 
-                annotate.errorLabel(
-                    $(el),
-                    "This contrast is insufficient at this size. Use a " +
-                    "background color of " + suggestedBgColor + " instead",
-                    contrastRatio.toFixed(2));
-            } else {
-                // Build a value for our `seenColors` set and report the color
-                // if we have not seen it yet
-                let entry = axs.utils.colorToString(fgColor) + "/" +
-                            axs.utils.colorToString(bgColor);
-
+            if (!axs.utils.isLowContrast(contrastRatio, style)) {
+                // For acceptable contrast values, we don't show ratios if
+                // they have been presented already
                 if (!seenColors.has(entry)) {
                     annotate
-                        .label($(el), contrastRatio.toFixed(2))
+                        .label($(el), contrastRatio)
                         .addClass("tota11y-label-success");
-                    seenColors.add(entry);
+                }
+            } else {
+                // We display errors multiple times for emphasis
+                annotate.errorLabel(
+                    $(el),
+                    "This contrast is insufficient at this size.",
+                    contrastRatio);
+
+                if (!seenColors.has(entry)) {
+                    // We do not show duplicates in the errors panel, however,
+                    // to keep the output from being overwhelming
+                    this._registerError(
+                        {fgColor, bgColor, contrastRatio, style},
+                        el);
                 }
             }
+
+            seenColors.add(entry);
         });
     }
 
