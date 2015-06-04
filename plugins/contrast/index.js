460904bf74a176aba7cffb328bd41f803693ddb1
Jordan Scales <scalesjordan@gmail.com>
2015-06-04 12:11:22 -0400

    compute required ratio earlier
    This allows us to include it in the seenColors "entry," so we can report
    errors even if the combo was okay at a larger size earlier

--- plugins/contrast/index.js
+++ plugins/contrast/index.js
@@ -22,23 +22,18 @@ class ContrastPlugin extends Plugin {
     }
 
     _registerError(options, el) {
-        // Suggest colors
+        // Suggest colors at an "AA" level
         let suggestedColors = axs.utils.suggestColors(
             options.bgColor,
             options.fgColor,
             options.contrastRatio,
             options.style).AA;
 
-        // We'll suggest colors at an "AA" level.
-        // 3.0 for large fonts, 4.5 for small fonts.
-        let requiredRatio = axs.utils.isLargeFont(options.style) ?
-            "3.0" : "4.5";  // Use strings to prevent rounding
-
         let templateData = {
             fgColorHex: axs.utils.colorToString(options.fgColor),
             bgColorHex: axs.utils.colorToString(options.bgColor),
             contrastRatio: options.contrastRatio,
-            requiredRatio: requiredRatio,
+            requiredRatio: options.requiredRatio,
             suggestedFgColorHex: suggestedColors.fg,
             suggestedBgColorHex: suggestedColors.bg,
             suggestedColorsRatio: suggestedColors.contrast
@@ -78,10 +73,16 @@ class ContrastPlugin extends Plugin {
             let contrastRatio = axs.utils.calculateContrastRatio(
                 fgColor, bgColor).toFixed(2);
 
-            // Build a value for our `seenColors` set and report the color
+            // Calculate required ratio based on size
+            // Using strings to prevent rounding
+            let requiredRatio = axs.utils.isLargeFont(style) ?
+                "3.0" : "4.5";
+
+            // Build a value for our `seenColors` map and report the color
             // if we have not seen it yet
             let entry = axs.utils.colorToString(fgColor) + "/" +
-                        axs.utils.colorToString(bgColor);
+                        axs.utils.colorToString(bgColor) + "/" +
+                        requiredRatio;
 
             if (!axs.utils.isLowContrast(contrastRatio, style)) {
                 // For acceptable contrast values, we don't show ratios if
@@ -101,7 +102,7 @@ class ContrastPlugin extends Plugin {
                     // We do not show duplicates in the errors panel, however,
                     // to keep the output from being overwhelming
                     let errorEntry = this._registerError(
-                        {fgColor, bgColor, contrastRatio, style},
+                        {fgColor, bgColor, contrastRatio, style, requiredRatio},
                         el);
 
                     seenColors[entry] = errorEntry;
