6294568c3ecd6a33b77d5d41425092b89bd8a9ed
Jordan Scales <scalesjordan@gmail.com>
2015-06-09 09:50:40 -0400

    Updated tota11y.min.js and disabled bookmarklet for non-Chrome browsers
    Summary:
    Heard some complaints of the bookmarklet not working correctly in Firefox. I can
    confirm this on my end as well, and I suspect that the URL might just be too long.
    
    Until I have time to figure this out, I've disabled the bookmarklet secton for
    non-chrome browsers. Sadface.
    
    Also added id's to two headings.
    
    Test Plan:
    . Load page in Chrome
    . See Bookmarklet is present under #Installation
    . See Bookmarklet works correctly
    
    . Load page in FF
    . See Bookmarklet section is missing
    
    Reviewers: riley, kevinb
    
    Differential Revision: https://phabricator.khanacademy.org/D18501

--- js/script.js
+++ js/script.js
@@ -10,15 +10,19 @@ document.addEventListener("touchstart", function(e) {
     }
 });
 
-// Load the bookmarklet
-var request = new XMLHttpRequest();
-request.open("GET", "./js/tota11y.min.js", true);
+if (/chrome/.test(navigator.userAgent.toLowerCase())) {
+    // Load the bookmarklet
+    var request = new XMLHttpRequest();
+    request.open("GET", "./js/tota11y.min.js", true);
 
-request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-        var bookmarklet = "javascript:" + encodeURI(request.responseText);
-        document.querySelector(".bookmarklet").href = bookmarklet;
-    }
-};
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var bookmarklet = "javascript:" + request.responseText;
+            document.querySelector(".bookmarklet").href = bookmarklet;
+        }
+    };
 
-request.send();
+    request.send();
+} else {
+    document.querySelector(".bookmarklet-container").style.display = "none";
+}
