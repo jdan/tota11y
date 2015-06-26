dfbbb8e0bd6f46caf9a27be96ddc26b4cb0bf1fc
Jordan Scales <scalesjordan@gmail.com>
2015-06-26 15:21:58 -0400

    Refactored unit tests to share jsdom and plugin instances
    Summary:
    By placing two tests side by side, I discovered that the `require` cache was
    causing a mismatch between the jQuery used by plugins and the jQuery test
    authors were given access to.
    
    I've rearranged the jsdom and plugin instancing to now occur in mocha's magic
    `before` method, allow us to use these instances in all of our tests. Should
    speed things up a bit, too, but I have not run any benchmarks.
    
    Test Plan: . npm test
    
    Reviewers: riley
    
    Differential Revision: https://phabricator.khanacademy.org/D18904

---
+++ test/landmarks-test.js
@@ -0,0 +1,54 @@
+/**
+ * Tests for the "Landmarks" plugin
+ */
+
+let assert = require("assert");
+let mockDom = require("./mock-dom");
+
+describe("Landmarks plugin", function() {
+    let dom = null;
+    let plugin = null;
+
+    before(function(done) {
+        mockDom.createDom(function(domObj) {
+            // Assign `dom` to the object returned by createDom, which allows
+            // us to set the HTML of the virtual environment, query it, and
+            // close the window.
+            dom = domObj;
+
+            let LandmarksPlugin = require("../plugins/landmarks");
+            plugin = new LandmarksPlugin();
+
+            done();
+        });
+    });
+
+    it("should label tags with a role", function() {
+        dom.setHTML(`
+            <div id="main-div" role="main">
+                Hello, world!
+            </div>
+        `);
+
+        plugin.run();
+
+        assert(dom.$("#main-div").hasLabel());
+        assert(dom.$("#main-div").labelText() === "main");
+    });
+
+    it("should not label tags without a role", function() {
+        dom.setHTML(`
+            <div id="main-div">
+                Hello, world!
+            </div>
+        `);
+
+        plugin.run();
+
+        assert(!dom.$("#main-div").hasLabel());
+    });
+
+    after(function() {
+        dom.destroy();
+    });
+});
