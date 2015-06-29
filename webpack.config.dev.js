32429a0925dec9d5935385fcda01d0e71df55900
Guillermo Bonvehí <gbonvehi@mpf.gov.ar>
2015-06-29 12:14:55 -0300

    New webpack config (output without .min) and npm run dev
    Modified build script to run prod (was build before) and dev which calls
    webpack using a new dev config which produces a file without the .min.
    suffix.

---
+++ webpack.config.dev.js
@@ -0,0 +1,33 @@
+var path = require("path");
+var postcss = require("postcss");
+var webpack = require("webpack");
+
+// PostCSS plugin to append !important to every CSS rule
+var veryimportant = postcss.plugin("veryimportant", function() {
+    return function(css) {
+        css.eachDecl(function (decl) {
+            decl.important = true;
+        });
+    };
+});
+
+module.exports = {
+    entry: {
+        app: "./index.js"
+    },
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "tota11y.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel" },
+            { test: /\.handlebars$/, loader: "handlebars" },
+            {
+                test: /\.less$/,
+                loader: "style!css!postcss!autoprefixer?{browsers:['> 1%']}!less"
+            }
+        ]
+    },
+    postcss: [veryimportant]
+};
