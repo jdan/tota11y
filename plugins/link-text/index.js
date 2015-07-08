14cc3a60f036678f234ae7293722bb1b0d03d0b3
Jordan Scales <scalesjordan@gmail.com>
2015-07-08 12:17:20 -0400

    tailored error messages if the link has an image with invalid alt text

--- plugins/link-text/index.js
+++ plugins/link-text/index.js
@@ -68,40 +68,49 @@ class LinkTextPlugin extends Plugin {
         };
     }
 
-    reportError($el, unclearText) {
-        let description = `
-            The text <i>"${unclearText}"</i> is unclear without context and
-            may be confusing to screen readers. Consider rearranging the
-            <code>&lt;a&gt;&lt;/a&gt;</code> tags or including special screen
-            reader text.
-        `;
-        // TODO: A "show me how" link may be even more helpful
-
-        let entry = this.error(
-            "Link text is unclear", description, $el);
-
-        annotate.errorLabel(
-            $el, "", `Link text "${unclearText}" is unclear`, entry);
+    reportError($el, description, textContent) {
+        let entry = this.error("Link text is unclear", description, $el);
+        annotate.errorLabel($el, "",
+            `Link text "${textContent}" is unclear`, entry);
     }
 
     run() {
         $("a").each((i, el) => {
             let $el = $(el);
+
+            // Ignore the tota11y UI
             if ($el.parents(".tota11y").length) {
                 return;
             }
 
-            let report;
             // If this anchor contains a single image, we will test the
             // clarity of that image's alt text.
             if ($el.find("> img").length === 1) {
-                report = this.validateAltText($el.find("> img")[0]);
-            } else {
-                report = this.validateTextContent(el);
-            }
+                let report = this.validateAltText($el.find("> img")[0]);
 
-            if (!report.result) {
-                this.reportError($el, report.textContent);
+                if (!report.result) {
+                    let description = `
+                        The alt text for this link's image,
+                        <i>"${report.textContent}"</i>, is unclear without
+                        context and may be confusing to screen readers.
+                        Consider providing more detailed alt text.
+                    `;
+
+                    this.reportError($el, description, report.textContent);
+                }
+            } else {
+                let report = this.validateTextContent(el);
+
+                if (!report.result) {
+                    let description = `
+                        The text <i>"${report.textContent}"</i> is unclear
+                        without context and may be confusing to screen readers.
+                        Consider rearranging the <code>&lt;a&gt;&lt;/a&gt;
+                        </code> tags or including special screen reader text.
+                    `;
+
+                    this.reportError($el, description, report.textContent);
+                }
             }
         });
     }
