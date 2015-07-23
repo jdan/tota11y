f3ae01186b8de45f5660754769c9aa66ed830940
Jordan Scales <scalesjordan@gmail.com>
2015-07-23 10:33:17 -0400

    include mocha fixes to run babel+JSX

---
+++ test/babel-hook.js
@@ -0,0 +1,14 @@
+/**
+ * A mocha hook that will run before each test.
+ *
+ * This sets up babel with options, and stores our JSX transpile target.
+ */
+
+// Register all future "require"s with babel
+require("babel/register")({
+    // Let babel know to transpile JSX into E(...)
+    jsxPragma: "E",
+});
+
+// Store our custom JSX transpile target as a global
+E = require("../element.js");
