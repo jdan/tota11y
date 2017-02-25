988e25157ac4a1bf4d6209fb31f69b91405865a3
richhildebrand <richhildebrand@gmail.com>
2017-02-24 19:11:00 -0500

    show all

--- plugins/show-all/index.js
+++ plugins/show-all/index.js
@@ -1,6 +1,11 @@
-/**
- * A plugin to check for valid alternative representations for images
- */
+
+let AltTextPlugin = require("../alt-text");
+let ContrastPlugin = require("../contrast");
+let HeadingsPlugin = require("../headings");
+let LabelsPlugin = require("../labels");
+let LandmarksPlugin = require("../landmarks");
+let LinkTextPlugin = require("../link-text");
+
 
 let $ = require("jquery");
 let Plugin = require("../base");
@@ -16,59 +21,15 @@ class ShowAllPlugin extends Plugin {
         return "Activate all options at once.";
     }
 
-    reportError(el) {
-        let $el = $(el);
-        let src = $el.attr("src") || "..";
-        let title = "Image is missing alt text";
-        let $error = (
-            <div>
-                <p>
-                    This image does not have an associated "alt" attribute.
-                    Please specify the alt text for this image like so:
-                </p>
-
-                <pre><code>
-                    {`&lt;img src="${src}" alt="Image description"&gt`}
-                </code></pre>
-
-                <p>
-                    If the image is decorative and does not convey any
-                    information to the surrounding content, however, you may
-                    leave this "alt" attribute empty, or specify a "role"
-                    attribute with a value of "presentation."
-                </p>
-
-                <pre><code>
-                    {`&lt;img src="${src}" alt=""&gt;`}
-                    <br />
-                    {`&lt;img src="${src}" role="presentation"&gt;`}
-                </code></pre>
-            </div>
-        );
-
-        // Place an error label on the element and register it as an
-        // error in the info panel
-        let entry = this.error(title, $error, $el);
-        annotate.errorLabel($el, "", title, entry);
-    }
+    reportError(el) { }
 
     run() {
-        // Generate errors for any images that fail the Accessibility
-        // Developer Tools audit
-        let {result, elements} = audit("imagesWithoutAltText");
-
-        if (result === "FAIL") {
-            elements.forEach(this.reportError.bind(this));
-        }
-
-        // Additionally, label presentational images
-        $(`img[role="presentation"], img[alt=""]`).each((i, el) => {
-            // "Error" labels have a warning icon and expanded text on hover,
-            // but we add a special `warning` class to color it differently.
-            annotate
-                .errorLabel($(el), "", "This image is decorative")
-                .addClass("tota11y-label-warning");
-        });
+        new HeadingsPlugin().run();
+        new ContrastPlugin().run();
+        new LinkTextPlugin().run();
+        new LabelsPlugin().run();
+        new AltTextPlugin().run();
+        new LandmarksPlugin().run();
     }
 
     cleanup() {
