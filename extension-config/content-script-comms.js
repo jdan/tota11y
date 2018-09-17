ac1ddbbfadd87f401926e946ee8190a2cfcbf2f6
Nick Taylor <nick@iamdeveloper.com>
2018-09-16 22:31:47 -0400

    Now tota11y script loaded only when needed.

--- extension-config/content-script-comms.js
+++ extension-config/content-script-comms.js
@@ -1,15 +1,29 @@
+function toggleTota11y(payload, sendResponse) {
+    const tota11yToolbar = document.getElementById("tota11y-toolbar");
+
+    if (!tota11yToolbar) {
+        sendResponse({ type: "activate_tota11y"});
+        return;
+    }
+
+    const tota11yWindows = [...document.querySelectorAll(".tota11y")];
+
+    [...tota11yWindows, tota11yToolbar].forEach(element => {
+        element.style.display = payload.showTota11y ? "block" : "none";
+    });
+
+    sendResponse({ type: "tota11y_toggle_success" });
+}
+
 if (chrome && chrome.runtime) {
-  let tota11yToolbar;
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        switch (request.type) {
+            case "toggle_tota11y":
+                toggleTota11y(request.payload, sendResponse);
+                break;
 
-  chrome.runtime.onMessage.addListener(({ isTota11yEnabled }) => {
-      if (!tota11yToolbar) {
-          tota11yToolbar = document.getElementById("tota11y-toolbar");
-      }
-
-      const tota11yWindows = [...document.querySelectorAll(".tota11y")];
-
-      [...tota11yWindows, tota11yToolbar].forEach(element => {
-          element.style.display = isTota11yEnabled ? "block" : "none";
-      });
-  });
+            default:
+                break;
+        }
+    });
 }
--- extension-config/popup.js
+++ extension-config/popup.js
@@ -1,15 +1,37 @@
-document.getElementById("tota11y-enabler").addEventListener("click", e => {
-  const isTota11yEnabled = e.currentTarget.checked;
+function getActiveTab() {
+    return new Promise((resolve, reject) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          const activeTab = tabs.find(t => t.active);
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs.find(t => t.active);
+          resolve(activeTab);
+      });
+    });
+}
+
+function activateTota11y() {
+    console.log('activating tota11y...');
+    chrome.tabs.executeScript(null, { file: "tota11y.js" });
+    console.log('t0ta11y has been activated.');
+}
+
+document.getElementById("tota11y-toggler").addEventListener("click", async event => {
+    const activeTab = await getActiveTab();
 
     if (!activeTab) {
-      return;
+        return;
     }
 
-    chrome.tabs.sendMessage(activeTab.id, { isTota11yEnabled }, response => {
-      console.dir(response);
+    const showTota11y = event.target.checked;
+    const message = { type: 'toggle_tota11y', payload: { showTota11y } };
+
+    chrome.tabs.sendMessage(activeTab.id, message, response => {
+        switch(response.type) {
+            case 'activate_tota11y':
+                activateTota11y();
+                break;
+
+            default:
+                console.log("Page responded with", response);
+        }
     });
-  });
 });
