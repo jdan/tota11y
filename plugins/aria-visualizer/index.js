a7a728a553adad9363e8b8ddfd388b8e2d4fd98f
Eric Allen <eric@ericrallen.dev>
2019-03-21 11:45:04 -0400

    feat(plugin): update AriaVisualizer to support role annotations

--- plugins/aria-visualizer/index.js
+++ plugins/aria-visualizer/index.js
@@ -4,20 +4,41 @@
 
 let Plugin = require("../base");
 
+let annotate = require("../shared/annotate")("roles");
+
 // this will let us get a shorter info panel that just
 // lets the user know we are tracking their focus
 const PANEL_OPTIONS = {
-  statusPanelView: true,
-  disableAnnotation: true,
+  statusPanelView: true
 };
+
+const ATTRIBUTES = [
+    "role",
+    "aria-hidden"
+];
 
 require("./style.less");
 
+const formatAttributeForMethodName = (attribute) => {
+    return attribute.split("-").map(part => `${part[0].toUpperCase()}${part.substr(1)}`).join("");
+}
+
 class AriaVisualizer extends Plugin {
     constructor(...args) {
-      const options = Object.assign({}, args, { panel: PANEL_OPTIONS });
+        const options = Object.assign({}, args, { panel: PANEL_OPTIONS });
 
-      super(options);
+        super(options);
+
+        this.ariaAttributes = ATTRIBUTES.reduce((functionMap, attribute) => {
+            const methodSuffix = formatAttributeForMethodName(attribute);
+
+            functionMap[attribute] = {
+                enable: this[`start${methodSuffix}`],
+                disable: this[`stop${methodSuffix}`]
+            };
+
+            return functionMap;
+        }, {});
     }
 
     getTitle() {
@@ -25,24 +46,57 @@ class AriaVisualizer extends Plugin {
     }
 
     getDescription() {
-        return "See the effects of your aria-* attributes";
+        return "See the effects of your aria-* and other a11y attributes";
+    }
+
+    startAriaHidden(attribute) {
+        const ariaName = `aria-${attribute}`;
+
+        [...document.querySelectorAll(`[${ariaName}="true"]:not(.tota11y)`)].forEach((element) => {
+            if (!element.closest(".tota11y")) {
+                element.classList.add(`tota11y-${ariaName}-visualized`);
+            }
+        });
+    }
+
+    stopAriaHidden(attribute) {
+        const ariaName = `aria-${attribute}`;
+
+        const className = `tota11y-${ariaName}-visualized`;
+
+        [...document.querySelectorAll(`.${className}`)].forEach((element) => {
+            element.classList.remove(className);
+        });
+    }
+
+    startRole(attribute) {
+        [...document.querySelectorAll(`[${attribute}]:not(.tota11y)`)].forEach((element) => {
+            if (!element.closest(".tota11y")) {
+                annotate.label(
+                    element,
+                    `role: ${element.getAttribute("role")}`
+                );
+            }
+        });
+    }
+
+    stopRole() {
+        annotate.removeAll();
     }
 
     run() {
         // pop up our info panel to let the user know what we're doing
-        this.summary("Visualizing aria-*");
+        this.summary("Visualizing a11y attributes");
         this.panel.render();
 
-        [...document.querySelectorAll("[aria-hidden=\"true\"]:not(.tota11y)")].forEach((element) => {
-          if (!element.closest(".tota11y")) {
-            element.classList.add("tota11y-aria-hidden-visualized");
-          }
+        Object.keys(this.ariaAttributes).forEach((property) => {
+            this.ariaAttributes[property].enable(property);
         });
     }
 
     cleanup() {
-        [...document.querySelectorAll(".tota11y-aria-hidden-visualized")].forEach((element) => {
-          element.classList.remove("tota11y-aria-hidden-visualized");
+        Object.keys(this.ariaAttributes).forEach((property) => {
+            this.ariaAttributes[property].disable(property);
         });
     }
 }
