0766ae783cd85f9f9ce78317dd79e038445f3296
Bruce Lawson <bruce@brucelawson.co.uk>
2020-11-24 11:19:40 +0000

    test Zonk

--- plugins/a11y-text-wand/index.js
+++ plugins/a11y-text-wand/index.js
@@ -8,56 +8,55 @@ let Plugin = require("../base");
 require("./style.less");
 
 class A11yTextWand extends Plugin {
-    getTitle() {
-        return "Screen Reader Wand";
-    }
+  getTitle() {
+    return "Screen Reader Zonk";
+  }
 
-    getDescription() {
-        return "Hover over elements to view them as a screen reader would";
-    }
+  getDescription() {
+    return "Hover over Zonks to view them as a screen reader would";
+  }
 
-    run() {
-        // HACK(jordan): We provide a fake summary to force the info panel to
-        //     render.
-        this.summary(" ");
-        this.panel.render();
+  run() {
+    // HACK(jordan): We provide a fake summary to force the info panel to
+    //     render.
+    this.summary(" ");
+    this.panel.render();
 
-        $(document).on("mousemove.wand", function(e) {
-            let element = document.elementFromPoint(e.clientX, e.clientY);
+    $(document).on("mousemove.wand", function (e) {
+      let element = document.elementFromPoint(e.clientX, e.clientY);
 
-            let textAlternative = axs.properties.findTextAlternatives(
-                element, {});
+      let textAlternative = axs.properties.findTextAlternatives(element, {});
 
-            $(".tota11y-outlined").removeClass("tota11y-outlined");
-			$(element).addClass("tota11y-outlined");
+      $(".tota11y-outlined").removeClass("tota11y-outlined");
+      $(element).addClass("tota11y-outlined");
 
-			// append anything found in aria-describedby, as screen readers will //announce this too. It's a good way of adding accessible help text to form inputs— see https://developer.paciellogroup.com/blog/2014/12/using-aria-describedby-to-provide-helpful-form-hints/
-			const describedBy = $(element).attr("aria-describedby");
-			if (describedBy) {
-				let describedIDs=describedBy.split(/\s/);
+      // append anything found in aria-describedby, as screen readers will //announce this too. It's a good way of adding accessible help text to form inputs— see https://developer.paciellogroup.com/blog/2014/12/using-aria-describedby-to-provide-helpful-form-hints/
+      const describedBy = $(element).attr("aria-describedby");
+      if (describedBy) {
+        let describedIDs = describedBy.split(/\s/);
 
-				for (const describedId of describedIDs) {
-					const el = document.getElementById(describedId);
-					if (el) textAlternative += " "+ el.textContent;
-				  }
-			}
+        for (const describedId of describedIDs) {
+          const el = document.getElementById(describedId);
+          if (el) textAlternative += " " + el.textContent;
+        }
+      }
 
-            if (!textAlternative) {
-                $(".tota11y-info-section.active").html(
-                    <i className="tota11y-nothingness">
-                        No text visible to a screen reader
-                    </i>
-                );
-            } else {
-                $(".tota11y-info-section.active").text(textAlternative);
-            }
-        });
-    }
+      if (!textAlternative) {
+        $(".tota11y-info-section.active").html(
+          <i className="tota11y-nothingness">
+            No text visible to a screen reader
+          </i>
+        );
+      } else {
+        $(".tota11y-info-section.active").text(textAlternative);
+      }
+    });
+  }
 
-    cleanup() {
-        $(".tota11y-outlined").removeClass("tota11y-outlined");
-        $(document).off("mousemove.wand");
-    }
+  cleanup() {
+    $(".tota11y-outlined").removeClass("tota11y-outlined");
+    $(document).off("mousemove.wand");
+  }
 }
 
 module.exports = A11yTextWand;
