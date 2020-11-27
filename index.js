0e62d23d8951691ef1e5d0658d6c8d82bfd5c924
Bruce Lawson <bruce@brucelawson.co.uk>
2020-11-27 14:03:01 +0000

    change 'experimental' to 'developers'

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ class Toolbar {
             $experimentalPlugins = (
                 <li>
                     <div className="tota11y-plugins-separator">
-                        Experimental
+                        Developers:
                     </div>
                     <ul>
                       {
