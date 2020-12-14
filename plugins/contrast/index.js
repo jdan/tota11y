c46d9d27dc8f8df440dcdc5cad7c39441a6f8b0a
Bruce Lawson <bruce@brucelawson.co.uk>
2020-12-14 16:38:35 +0000

    Make it resposnive to 200% browser zoom (scrollable vertically)

--- plugins/contrast/index.js
+++ plugins/contrast/index.js
@@ -13,193 +13,209 @@ let descriptionTemplate = require("./error-description.handlebars");
 require("./style.less");
 
 class ContrastPlugin extends Plugin {
-    constructor() {
-        super();
-        // List of original colors for elements with insufficient contrast.
-        // Used to restore original colors in cleanup.
-        this.preservedColors = [];
-    }
+  constructor() {
+    super();
+    // List of original colors for elements with insufficient contrast.
+    // Used to restore original colors in cleanup.
+    this.preservedColors = [];
+  }
 
-    getTitle() {
-        return "Contrast";
-    }
+  getTitle() {
+    return "Contrast";
+  }
 
-    getDescription() {
-        return "Labels elements with insufficient contrast";
-    }
+  getDescription() {
+    return "Labels elements with insufficient contrast";
+  }
 
-    addError({style, fgColor, bgColor, contrastRatio, requiredRatio}, el) {
-        // Suggest colors at an "AA" level
-        let suggestedColors = axs.color.suggestColors(
-            bgColor,
-            fgColor,
-            { AA: requiredRatio }).AA;
+  addError({ style, fgColor, bgColor, contrastRatio, requiredRatio }, el) {
+    // Suggest colors at an "AA" level
+    let suggestedColors = axs.color.suggestColors(bgColor, fgColor, {
+      AA: requiredRatio,
+    }).AA;
 
-        let templateData = {
-            fgColorHex: axs.color.colorToString(fgColor),
-            bgColorHex: axs.color.colorToString(bgColor),
-            contrastRatio: contrastRatio,
-            requiredRatio: requiredRatio,
-            suggestedFgColorHex: suggestedColors.fg,
-            suggestedBgColorHex: suggestedColors.bg,
-            suggestedColorsRatio: suggestedColors.contrast
-        };
+    let templateData = {
+      fgColorHex: axs.color.colorToString(fgColor),
+      bgColorHex: axs.color.colorToString(bgColor),
+      contrastRatio: contrastRatio,
+      requiredRatio: requiredRatio,
+      suggestedFgColorHex: suggestedColors.fg,
+      suggestedBgColorHex: suggestedColors.bg,
+      suggestedColorsRatio: suggestedColors.contrast,
+    };
 
-        // Add click handler to preview checkbox
-        let $description = $(descriptionTemplate(templateData));
-        let originalFgColor = style.color;
-        let originalBgColor = style.backgroundColor;
+    // Add click handler to preview checkbox
+    let $description = $(descriptionTemplate(templateData));
+    let originalFgColor = style.color;
+    let originalBgColor = style.backgroundColor;
 
-        $description.find(".preview-contrast-fix").click((e) => {
-            if ($(e.target).prop("checked")) {
-                // Set suggested colors
-                $(el).css("color", suggestedColors.fg);
-                $(el).css("background-color", suggestedColors.bg);
-            } else {
-                // Set original colors
-                $(el).css("color", originalFgColor);
-                $(el).css("background-color", originalBgColor);
-            }
-        });
+    $description.find(".preview-contrast-fix").click((e) => {
+      if ($(e.target).prop("checked")) {
+        // Set suggested colors
+        $(el).css("color", suggestedColors.fg);
+        $(el).css("background-color", suggestedColors.bg);
+      } else {
+        // Set original colors
+        $(el).css("color", originalFgColor);
+        $(el).css("background-color", originalBgColor);
+      }
+    });
 
-        return this.error(
-            titleTemplate(templateData),
-            $description,
-            $(el));
-    }
+    return this.error(titleTemplate(templateData), $description, $(el));
+  }
 
-    run() {
-        // A map of fg/bg color pairs that we have already seen to the error
-        // entry currently present in the info panel
-        let combinations = {};
+  run() {
+    // A map of fg/bg color pairs that we have already seen to the error
+    // entry currently present in the info panel
+    let combinations = {};
 
-        $("*").each((i, el) => {
-            // Only check elements with a direct text descendant
-            if (!axs.properties.hasDirectTextDescendant(el)) {
-                return;
-            }
+    $("*").each((i, el) => {
+      // Only check elements with a direct text descendant
+      if (!axs.properties.hasDirectTextDescendant(el)) {
+        return;
+      }
 
-            // Ignore elements that are part of the tota11y UI
-            if ($(el).parents(".tota11y").length > 0) {
-                return;
-            }
+      // Ignore elements that are part of the tota11y UI
+      if ($(el).parents(".tota11y").length > 0) {
+        return;
+      }
 
-			// Ignore invisible elements
+      // Ignore invisible elements
 
-            if (axs.utils.elementIsTransparent(el) ||
-                axs.utils.elementHasZeroArea(el)) {
-                    return;
-            }
+      if (
+        axs.utils.elementIsTransparent(el) ||
+        axs.utils.elementHasZeroArea(el)
+      ) {
+        return;
+      }
 
-			let style = getComputedStyle(el);
+      let style = getComputedStyle(el);
 
-			// TODO also ignore 'visually hidden' things, eg https://www.a11yproject.com/posts/2013-01-11-how-to-hide-content/
+      // Also ignore 'visually hidden' things, eg https://www.a11yproject.com/posts/2013-01-11-how-to-hide-content/
 
-			const visuallyHidden = ((style.getPropertyValue("clip") == "rect(0px, 0px, 0px, 0px)") && (style.getPropertyValue("clip-path") == "inset(50%)") && (style.getPropertyValue("height") == "1px") && (style.getPropertyValue("overflow") == "hidden") &&  (style.getPropertyValue("position") == "absolute") && (style.getPropertyValue("white-space") == "nowrap") && (style.getPropertyValue("width") == "1px"))
+      const visuallyHidden =
+        style.getPropertyValue("clip") == "rect(0px, 0px, 0px, 0px)" && // even when zero, needs units
+        style.getPropertyValue("clip-path") == "inset(50%)" &&
+        style.getPropertyValue("height") == "1px" &&
+        style.getPropertyValue("overflow") == "hidden" &&
+        style.getPropertyValue("position") == "absolute" &&
+        style.getPropertyValue("white-space") == "nowrap" &&
+        style.getPropertyValue("width") == "1px";
 
-			if (visuallyHidden) {return};
+      if (visuallyHidden) {
+        return;
+      }
+      //
 
+      let bgColor = axs.utils.getBgColor(style, el);
+      let fgColor = axs.utils.getFgColor(style, el, bgColor);
 
-            let bgColor = axs.utils.getBgColor(style, el);
-			let fgColor = axs.utils.getFgColor(style, el, bgColor);
+      // Previous test used axs.utils.isLargeFont, which doesn't take bold text into account. This stolen from Mozilla under MPL 2.0 license https://searchfox.org/mozilla-central/source/devtools/shared/accessibility.js#23
 
-// Previous test used axs.utils.isLargeFont, which doesn't take bold text into account. This stolen from Mozilla uner MPL 2.0 license https://searchfox.org/mozilla-central/source/devtools/shared/accessibility.js#23
+      const isBoldText =
+        parseInt(style.getPropertyValue("font-weight"), 10) >= 600;
+      const size = parseFloat(style.getPropertyValue("font-size"));
 
-			const isBoldText = parseInt(style.getPropertyValue("font-weight"), 10) >= 600;
-			const size = parseFloat(style.getPropertyValue("font-size"));
+      const LARGE_TEXT = {
+        // CSS pixel value (constant) that corresponds to 14 point text size which defines large text when font text is bold (font weight is greater than or equal to 600).
+        BOLD_LARGE_TEXT_MIN_PIXELS: 18.66,
+        // CSS pixel value (constant) that corresponds to 18 point text size which defines large text for normal text (e.g. not bold).
+        LARGE_TEXT_MIN_PIXELS: 24,
+      };
 
-			const LARGE_TEXT = {
-				// CSS pixel value (constant) that corresponds to 14 point text size which defines large text when font text is bold (font weight is greater than or equal to 600).
-				BOLD_LARGE_TEXT_MIN_PIXELS: 18.66,
-				// CSS pixel value (constant) that corresponds to 18 point text size which defines large text for normal text (e.g. not bold).
-				LARGE_TEXT_MIN_PIXELS: 24,
-			  };
+      const isLargeText =
+        size >=
+        (isBoldText
+          ? LARGE_TEXT.BOLD_LARGE_TEXT_MIN_PIXELS
+          : LARGE_TEXT.LARGE_TEXT_MIN_PIXELS);
 
-  			const isLargeText = size >=(isBoldText
-      ? LARGE_TEXT.BOLD_LARGE_TEXT_MIN_PIXELS
-      : LARGE_TEXT.LARGE_TEXT_MIN_PIXELS);
+      // end Moz bit, need to replace the axs.utils below TODO
 
-// end Moz bit, need to replace the axs.utils below TODO
+      // Calculate required ratio based on size
+      // Using strings to prevent rounding
+      //          let requiredRatio = axs.utils.isLargeFont(style) ?
+      //				3.0 : 4.5;
 
-            // Calculate required ratio based on size
-            // Using strings to prevent rounding
-//          let requiredRatio = axs.utils.isLargeFont(style) ?
-//				3.0 : 4.5;
+      let requiredRatio = isLargeText ? 3.0 : 4.5;
 
-			let requiredRatio = isLargeText ?
-				3.0 : 4.5;
+      let contrastRatio = axs.color
+        .calculateContrastRatio(fgColor, bgColor)
+        .toFixed(2);
 
-            let contrastRatio = axs.color.calculateContrastRatio(
-				fgColor, bgColor).toFixed(2);
+      //				console.log(contrastRatio+" / reqd="+ requiredRatio);
 
-//				console.log(contrastRatio+" / reqd="+ requiredRatio);
+      // Build a key for our `combinations` map and report the color
+      // if we have not seen it yet
+      let key =
+        axs.color.colorToString(fgColor) +
+        "/" +
+        axs.color.colorToString(bgColor) +
+        "/" +
+        requiredRatio;
 
+      if (contrastRatio > requiredRatio) {
+        // For acceptable contrast values, we don't show ratios if
+        // they have been presented already
+        if (!combinations[key]) {
+          annotate
+            .label($(el), contrastRatio)
+            .addClass("tota11y-label-success");
 
-            // Build a key for our `combinations` map and report the color
-            // if we have not seen it yet
-            let key = axs.color.colorToString(fgColor) + "/" +
-                        axs.color.colorToString(bgColor) + "/" +
-                        requiredRatio;
+          // Add the key to the combinations map. We don't have an
+          // error to associate it with, so we'll just give it the
+          // value of `true`.
+          combinations[key] = true;
+        }
+      } else {
+        if (!combinations[key]) {
+          // We do not show duplicates in the errors panel, however,
+          // to keep the output from being overwhelming
+          let error = this.addError(
+            {
+              style,
+              fgColor,
+              bgColor,
+              contrastRatio,
+              requiredRatio,
+            },
+            el
+          );
 
-            if (contrastRatio > requiredRatio ) {
-                // For acceptable contrast values, we don't show ratios if
-                // they have been presented already
-                if (!combinations[key]) {
-                    annotate
-                        .label($(el), contrastRatio)
-                        .addClass("tota11y-label-success");
+          // Save original color so it can be restored on cleanup.
+          this.preservedColors.push({
+            $el: $(el),
+            fg: style.color,
+            bg: style.backgroundColor,
+          });
 
-                    // Add the key to the combinations map. We don't have an
-                    // error to associate it with, so we'll just give it the
-                    // value of `true`.
-                    combinations[key] = true;
-                }
-            } else {
-                if (!combinations[key]) {
-                    // We do not show duplicates in the errors panel, however,
-                    // to keep the output from being overwhelming
-                    let error = this.addError({
-                        style,
-                        fgColor,
-                        bgColor,
-                        contrastRatio,
-                        requiredRatio,
-                    }, el);
+          combinations[key] = error;
+        }
 
-                    // Save original color so it can be restored on cleanup.
-                    this.preservedColors.push({
-                        $el: $(el),
-                        fg: style.color,
-                        bg: style.backgroundColor,
-                    });
+        // We display errors multiple times for emphasis. Each error
+        // will point back to the entry in the info panel for that
+        // particular color combination.
+        //
+        // TODO: The error entry in the info panel will only highlight
+        // the first element with that color combination
+        annotate.errorLabel(
+          $(el),
+          contrastRatio,
+          "This contrast is insufficient at this size.",
+          combinations[key]
+        );
+      }
+    });
+  }
 
-                    combinations[key] = error;
-                }
+  cleanup() {
+    // Set all elements to original color
+    this.preservedColors.forEach((entry) => {
+      entry.$el.css("color", entry.fg);
+      entry.$el.css("background-color", entry.bg);
+    });
 
-                // We display errors multiple times for emphasis. Each error
-                // will point back to the entry in the info panel for that
-                // particular color combination.
-                //
-                // TODO: The error entry in the info panel will only highlight
-                // the first element with that color combination
-                annotate.errorLabel(
-                    $(el),
-                    contrastRatio,
-                    "This contrast is insufficient at this size.",
-                    combinations[key]);
-            }
-        });
-    }
-
-    cleanup() {
-        // Set all elements to original color
-        this.preservedColors.forEach((entry) => {
-            entry.$el.css("color", entry.fg);
-            entry.$el.css("background-color", entry.bg);
-        });
-
-        annotate.removeAll();
-    }
+    annotate.removeAll();
+  }
 }
 
 module.exports = ContrastPlugin;
